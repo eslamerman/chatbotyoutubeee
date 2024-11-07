d2c212546e08471a8a25275de6f3872f94781065
eslamerman <43751216+eslamerman@users.noreply.github.com>
2024-11-07 09:08:44 +0200

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,25 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# Page Configuration
+st.set_page_config(page_title="MathGPT", page_icon="📐")
+
+# Sidebar
+with st.sidebar:
+    st.markdown("### We stand on the side of peace ✌️ 🕊️ ✨")
+    st.button("Login with Google")
+    st.button("Subscribe now!")
+    st.markdown("""
+    Do you need human-level intelligence? Send us your problem in [this link](#).
+    Cost of assistance depending on complexity, starting EE from $10 USD
+    """)
+
+# Main Content
+st.title("MathGPT")
+st.subheader("Use GPT to solve math problems")
+st.markdown("Follow [Twitter](#)  |  Contact: [mathgpt.app@gmail.com](mailto:mathgpt.app@gmail.com)")
+
+# Information Section
+st.info("Dear user, to use the app you need to login and subscribe.", icon="📦")
+
+# Blog Link
+st.markdown("#### Blog 👉 [📘](#)")
